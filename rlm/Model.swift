2dc9cd31d0bc24985efe069e72aab203aea2b517
pavel <pavel>
2017-08-09 22:49:50 +0300

    first commit, had fan with Realm

---
+++ rlm/Model.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  rlm
+//
+//  Created by pavel on 8/9/17.
+//  Copyright © 2017 pavel. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Person: Object {
+    dynamic var name = ""
+    dynamic var age = 0
+    dynamic var race = ""
+}
--- rlm/ViewController.swift
+++ rlm/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import RealmSwift
+class ViewController: UIViewController,
+    UITableViewDataSource,
+    UITableViewDelegate,
+    UITextFieldDelegate {
+    var people = [Person]()
+    let realm = try! Realm()
 
-class ViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var search: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        search.delegate = self
+        queryPeople()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = people[indexPath.row].name
+        return cell
     }
 
 
+    func addPeople() {
+        let person = Person()
+        person.name = "John"
+        person.race = "Белый"
+        person.age = 30
+        try! realm.write {
+            realm.add(person)
+        }
+
+    }
+
+    func queryPeople() {
+        let allPeople = realm.objects(Person.self)
+        for human in allPeople {
+            self.people.append(human)
+        }
+        self.tableView.reloadData()
+    }
+
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+
+        self.people.removeAll()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            if let text = textField.text?.characters, text.count > 0 {
+                let predicate = NSPredicate(format: "name CONTAINS [j] %@", textField.text!)
+                let fiteredPeople = self.realm.objects(Person.self).filter(predicate)
+                for each in fiteredPeople {
+                    self.people.append(each)
+                }
+                self.tableView.reloadData()
+            } else {
+                self.queryPeople()
+            }
+        }
+        return true
+    }
 }
 
